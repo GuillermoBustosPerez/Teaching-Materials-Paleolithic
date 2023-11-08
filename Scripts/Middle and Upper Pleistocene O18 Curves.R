fa625bbf7799c50f04d9c8c6b9495681ba8fa37b
GuillermoBustosPerez <guillermo.willbustos@gmail.com>
2023-11-08 15:09:56 +0100

    Added O18 curves of Upper and Middle Pleistocene

---
+++ Scripts/Middle and Upper Pleistocene O18 Curves.R
@@ -0,0 +1,46 @@
+library(tidyverse); library(gsloid)
+
+mis_last_250ka <-
+  LR04_MISboundaries %>% filter(LR04_Age_ka_start >= 11.7 & LR04_Age_ka_end <= 300)
+
+
+lisiecki2005 %>% filter(Time >= 11.7 & Time <= 300) %>% 
+  mutate(Periodo = case_when(
+    Time < 774 & Time >= 126 ~ "Pleist. Medio (773 - 126 Ka)",
+    Time < 126 ~ "Pleist. Superior (126 - 11.7 Ka)")) %>% 
+  
+  ggplot(aes((Time), d18O, color = Periodo)) +
+  
+  annotate("rect", 
+           xmin = mis_last_250ka$LR04_Age_ka_end, 
+           xmax = mis_last_250ka$LR04_Age_ka_start, 
+           ymin = -Inf, 
+           ymax = Inf,
+           alpha = .2,
+           fill = rep(c("grey70", "white"), 
+                      nrow(mis_last_250ka)/2)) +
+  annotate("text", 
+           label = mis_last_250ka$label_MIS, 
+           x =     mis_last_250ka$LR04_Age_ka_mid,
+           y = c(rep(3, 4), 
+                 seq(3.1, 2.8, length.out = 7),
+                 rep(3, 3)),
+           size = 4.5) +
+  
+  geom_path(aes(group = 1), linewidth = 1) +
+  scale_y_reverse(name = bquote(delta^18*O),
+                  breaks = seq(2, 5, 0.5), lim = c(5.5, 2)) +
+  xlab("Años BP Ka") +
+  scale_color_manual(values = c("purple", "orange")) +
+  labs(caption = "Datos de Lisiecki & Raymo, 2005") +
+  scale_x_continuous(breaks = seq(20, 300, 20), lim = c(11.7, 300)) +
+  theme_light() +
+  guides(color = guide_legend(nrow = 1)) +
+  theme(
+    legend.position = "top",
+    axis.text = element_text(color = "black", size = 12),
+    axis.title = element_text(color = "black", size = 13, face = "bold"),
+    legend.text = element_text(color = "black", size = 13),
+    legend.title = element_text(color = "black", size = 13.5, face = "bold"),
+  )
+
