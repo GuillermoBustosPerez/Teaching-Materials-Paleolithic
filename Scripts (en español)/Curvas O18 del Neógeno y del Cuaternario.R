b40a0e14f5cbbde63ec0f593354c30eec4d73ff2
GuillermoBustosPerez <guillermo.willbustos@gmail.com>
2023-12-20 09:43:08 +0100

    O18 Curves of the neogene

---
+++ Scripts (en español)/Curvas O18 del Neógeno y del Cuaternario.R
@@ -0,0 +1,53 @@
+library(tidyverse); library(gsloid)
+
+#### Curva O18 del Neógeno ####
+# Curva O18 Neógeno 
+lisiecki2005 %>% filter(Time >= 2800) %>% 
+  ggplot(aes((Time/1000), d18O)) +
+  geom_line(color = "blue") +
+  scale_y_reverse(name = bquote(delta^18*O),
+                  breaks = seq(2, 5.5, 0.5), lim = c(5.5, 2)) +
+  xlab("Años Ma") +
+  geom_hline(yintercept = 3.75, linetype = "dashed") +
+  geom_hline(yintercept = 2.75, linetype = "dashed") +
+  ggtitle("Neógeno (Previo al Cuaternario)") +
+  scale_x_reverse(breaks = seq(2.8, 5.3, 0.2)) +
+  geom_label(aes(x = 3, y = 5.4, 
+                 label = "(Lisiecki & Raymo 2005)"),
+             color = "black", fill = "white", size = 4, alpha = 0.5) +
+  theme_light() +
+  theme(
+    axis.text = element_text(color = "black", size = 11),
+    axis.title = element_text(color = "black", size = 11, face = "bold")
+  )
+
+ggsave(
+  "Images-español/Curva O18 del Neógeno.jpg",
+  device = "jpg",
+  dpi = 1200,
+  width = 32,
+  height = 10,
+  units = "cm"
+)
+
+##### O18 Curve of the Quaternary #####
+# Curva O18 Cuaternario 
+lisiecki2005 %>% filter(Time <= 2800) %>% 
+  ggplot(aes((Time/1000), d18O)) +
+  geom_line(color = "blue") + 
+  scale_y_reverse(name = bquote(delta^18*O),
+                  breaks = seq(2, 5.5, 0.5), lim = c(5.5, 2)) +
+  xlab("Años Ma") +
+  ggtitle("Cuaternario") +
+  scale_x_continuous(breaks = seq(0, 2.8, 0.2)) +
+  geom_hline(yintercept = 5, linetype = "dashed") +
+  geom_hline(yintercept = 3, linetype = "dashed") +
+  theme_light() +
+  geom_label(aes(x = 2.65, y = 5.4, 
+                 label = "(Lisiecki & Raymo 2005)"),
+             color = "black", fill = "white", size = 4, alpha = 0.5) +
+  theme(
+    axis.text = element_text(color = "black", size = 11),
+    axis.title = element_text(color = "black", size = 11, face = "bold")
+  )
+
